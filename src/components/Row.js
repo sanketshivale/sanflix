8af935d92534d3ca1a7ddc47c83fecdb9257a01f
shivalesanket <shivalesanket21@gmail.com>
2022-09-26 22:22:52 +0530

    On branch master Changes to be committed: 	modified:   src/components/Row.js

--- src/components/Row.js
+++ src/components/Row.js
@@ -25,7 +25,8 @@ function Row({title, fetchUrl, isLargeRow}){
             <h2 className='row_title'>{title}</h2>
             <div className='row_posters'>
                 {movies.map(movie =>(
-                    <img key={movie.id} className={`row_poster ${isLargeRow && "row_posterLarge" }`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path }`} alt={movie.name}></img>
+                    <img key={movie.id} className={`row_poster ${isLargeRow && "row_posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}></img>
+                  
                 ))}
     
             </div>
